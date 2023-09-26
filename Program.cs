521567b18fd388fa7c3a5e02db390e12e55559b8
Timebinder12 <shanereed28@gmail.com>
2023-09-26 18:07:31 -0400

    Removed startup file and updated Program.cs in accordance to .net 6.0

--- Program.cs
+++ Program.cs
@@ -1,45 +1,53 @@
 using JellyPagesMasterDetailApp.Models;
+using JellyPagesMasterDetailApp.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using JellyPagesMasterDetailApp;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
 
-namespace JellyPagesMasterDetailApp {
-    public class Program 
-     {
-        public static void Main(string[] args) 
+namespace JellyPagesMasterDetailApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope()) 
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<JellyPagesMasterDetailAppContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("JellyPagesMasterDetailAppContext") ?? throw new InvalidOperationException("Connection string 'PDMasterDetailContext' not found.")));
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-
-                try 
-                {
-                    SeedData.Initialize(services);
-                }
-                catch(Exception ex) 
-                { 
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occured seeding the DB.");
-                }
+                SeedData.Initialize(services);
             }
 
-            host.Run();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
 
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
